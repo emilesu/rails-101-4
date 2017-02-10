d19b04ee82ecc8fe19f9084840c665a78798758a
emilesu <emilesu@qq.com>
2017-02-10 14:19:00 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -41,6 +41,31 @@ class GroupsController < ApplicationController
       redirect_to groups_path, notice: "删除成功"
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "成功加入本讨论组"
+    else
+      flash[:warning] = "你已经是本讨论组成员了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论组"
+    else
+      flash[:warning] = "你不是讨论组成员"
+    end
+
+    redirect_to group_path(@group)
+  end
 
 
   private
